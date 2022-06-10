397b51886d7135507cc34b95130cde6429dc8767
Benjamin Bascary <benjaminbascary@gmail.com>
2022-06-09 21:12:33 -0300

    Commits basic express server with template working
    This commit adds the express configured up and running and the template engine ready to add features.

---
+++ app.js
@@ -0,0 +1,62 @@
+import { engine as expressHandlebars } from 'express-handlebars';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import fortunes from './data/fortuneCookies.js';
+import express from 'express';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express()
+
+const port = process.env.PORT || 3000;
+
+/**
+ * STATIC middleware
+ */
+
+app.use(express.static(__dirname + '/public'));
+
+
+
+/**
+ * HandleBars handler
+ */
+
+app.engine('handlebars', expressHandlebars({
+	defaultLayout : 'main'
+}))
+app.set('view engine', 'handlebars');
+
+/**
+ * Routes
+ */
+
+app.get('/', (req, res) => {
+	res.render('home');
+})
+
+app.get('/about', (req, res) => {
+	const randomFortune = fortunes[Math.floor(Math.random()*fortunes.length)]
+	res.render('about', {fortune: randomFortune});
+
+})
+
+/**
+ * 404 and 500 custom pages.
+ */
+
+app.use((req, res) => {
+	res.status(404);
+	res.render('404');
+})
+
+app.use((err, req, res, next) => {
+	console.error(err);
+	res.status(500);
+	res.render('500')
+})
+
+app.listen(port, () => {
+  console.log(`Express running on port ${port};\n` + `Press Ctrl-C to terminate the process.`);
+})
+
